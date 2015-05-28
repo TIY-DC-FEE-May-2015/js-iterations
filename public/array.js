23fa0a5d6a2eef56d4f4e7020ac3a28fab1edbb5
Ghaea <ghaea.tam@gmail.com>
2015-05-28 14:17:41 -0400

    learned arrays

--- public/array.js
+++ public/array.js
@@ -3,7 +3,9 @@
   It should return the value of the number times 2.
 */
 var doubler = function(number) {
-  
+    var double = number * 2
+
+  return double
 }
 
 /*
@@ -12,6 +14,14 @@ var doubler = function(number) {
 */
 var arrayDoubler = function(array) {
   
+  for (var i in array) {
+
+    array[i] = array[i] * 2
+
+  }
+
+  return array;
+  
 }
 
 /*
@@ -19,7 +29,9 @@ var arrayDoubler = function(array) {
   It should return the first number, divided by the second number.
 */
 var divide = function(numerator, denominator) {
+  var divided = numerator/denominator;
 
+  return divided
 }
 
 /*
@@ -28,6 +40,12 @@ var divide = function(numerator, denominator) {
 */
 var arrayDivide = function(array, denominator) {
   
+  for (var i in array) {
+    array[i] = array[i]/denominator;
+  }
+
+  return array
+
 }
 
 /*
@@ -35,6 +53,8 @@ var arrayDivide = function(array, denominator) {
   It should return the sum of the two numbers.
 */
 var sum = function(a, b) {
+
+  return a + b;
   
 }
 
@@ -43,7 +63,13 @@ var sum = function(a, b) {
   It should return the sum of all the members in the array.
 */
 var arraySum = function(array) {
-  
+
+  var sum = 0;
+
+  for (var i = 0; i < array.length; i++) {
+    sum = array[i] + sum
+  }
+  return sum
 }
 
 /*
@@ -51,6 +77,12 @@ var arraySum = function(array) {
   It should return the lower of the two numbers.
 */
 var minimum = function(a, b) {
+
+  if (a < b) {
+    return a
+  }
+
+  return b
   
 }
 
@@ -60,7 +92,25 @@ var minimum = function(a, b) {
   If the array has a length = 0, it should return undefined.
 */
 var arrayMinimum = function(array) {
-  
+    
+    if (array.length === 0) {
+        
+        return undefined
+        
+    }
+    
+    else {
+        var min = array[0]
+    
+        for(var i = 0; i < array.length; i++) {
+        
+            if (array[i] < min) {
+                min = array[i]
+            }
+        }
+    return min
+    }
+
 }
 
 /*
@@ -69,6 +119,16 @@ var arrayMinimum = function(array) {
   Otherwise, it should return false.
 */
 var endsInPeriod = function(string) {
+
+    var split = string.split("")
+    
+    var last = split.length - 1
+    
+    if (split[last] === ".") {
+        return true
+    }
+    
+    return false
   
 }
 
@@ -78,6 +138,19 @@ var endsInPeriod = function(string) {
     from the original array which end with a period.
 */
 var arrayEndsInPeriod = function(array) {
+  var arrayPeriod = []
+  
+  for (var i = 0; i < array.length; i++) {
+      
+      var word = endsInPeriod(array[i])
+      
+      if ( word === true) {
+          
+          arrayPeriod.push(array[i])
+      }
+
+  }
+  return arrayPeriod 
   
 }
 
@@ -87,6 +160,11 @@ var arrayEndsInPeriod = function(array) {
   Otherwise, it should return false.
 */
 var multiple3 = function(number) {
+
+  if (number % 3 === 0) {
+    return true
+  }
+  return false
   
 }
 
@@ -97,6 +175,20 @@ var multiple3 = function(number) {
 */
 var arrayMultiple3 = function(array) {
   
+  var numMultiple3 = []
+  
+  for (var i = 0; i < array.length; i++) {
+      
+      var number = multiple3(array[i]) 
+      
+      if( number === true) {
+          
+          numMultiple3.push(array[i])
+      }
+  }
+  
+  return numMultiple3
+
 }
 
 
