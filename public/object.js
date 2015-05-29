e2d5e9b7c97840fd386e4f2414e1a3d3595bb46e
BBSR <scottamross@gmail.com>
2015-05-29 13:48:30 -0400

    object achieved

--- public/object.js
+++ public/object.js
@@ -1,4 +1,4 @@
-/*
+/*  
   This function should accept an array as a parameter.
   It should return an object that contains the number of times each unique
     value showed up in the array.
@@ -34,13 +34,18 @@ var unique = function(array) {
     var itemName = array[i]
     obj[itemName] = itemName
 }
-    
+
+// first approach - works
+
     for (var itemName in obj) {
      arr.push(obj[itemName])   
     }
-return arr
-// Object.keys(itemName) - but don't know how to use it
 
+// Object.keys(itemName) - but don't know how to use it
+/*
+arr = Object.keys(itemName)
+*/
+return arr
 }
 
 
@@ -50,7 +55,7 @@ return arr
   It should return the value of the number times 3.
 */
 var tripler = function(number) {
-  
+  return number * 3 
 }
 
 /*
@@ -58,8 +63,22 @@ var tripler = function(number) {
   It should return the object with all of its *values* tripled.
 */
 var objectTripler = function(object) {
-  
+  for (var i in object) {
+ //perform the function on object[i] to change the VALUE. The KEY stays unaffected
+ //remember to set the key equal to a new value or nothing happens (i think)
+    object[i] = tripler(object[i])
+  }
+  return object
 }
+
+/*
+var test = {
+  a: 2,
+  b: 537,
+}
+
+console.log(objectTripler(test))
+*/
 
 /*
   This function should accept two objects as parameters.
@@ -69,7 +88,16 @@ var objectTripler = function(object) {
   For instance, ({ a: 1, b: 2 } and { b: 9, c: 3 }) would produce { a: 1, b: 2, c: 3 }
 */
 var extend = function(left, right) {
-  
+  //loop through left
+  for (var pizza in left) {
+  //send the contents of left to right
+    right[pizza] = left[pizza]
+  }
+  //shitcan left (if desired)
+  delete left
+  //return right
+
+  return right
 }
 
 /*
@@ -80,5 +108,19 @@ var extend = function(left, right) {
   For instance, [{a: 1, b: 2}, {a: 3, c: 4}] for "a" would produce [1, 3]
 */
 var pluck = function(arrayOfObjects, string) {
-  
+  var xavierSchool = []
+  var giftedYoungster = 0
+  //loop through the array, with objects as the var      
+  for (var i = 0; i < arrayOfObjects.length; i++) {
+    //loop each object
+    for (var giftedYoungster in arrayOfObjects[i]) {
+    //if it finds var string
+      if (giftedYoungster === string) {
+    //push that to a new array
+        xavierSchool.push(arrayOfObjects[i][giftedYoungster])
+      }
+    }
+  }
+  return xavierSchool
 }
+
